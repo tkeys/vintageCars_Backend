81a4cdf1fe992e6a137ddc8644599269a31c1ca1
Danilo Canguçu <daniloccangucu@gmail.com>
2024-03-14 14:56:04 +0100

    base project

---
+++ src/types/Condition.ts
@@ -0,0 +1,12 @@
+export enum Condition {
+  Mint = "Mint",
+  Restored = "Restored",
+  Original = "Original",
+  Classic = "Classic",
+  Collector = "Collector",
+  Project = "Project",
+  BarnFind = "Barn Find",
+  DailyDriver = "Daily Driver",
+  Concours = "Concours",
+  Excellent = "Excellent",
+}
