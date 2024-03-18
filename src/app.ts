f0e20d5463cbadfe547d9c03bc5b41be3a2ece53
Danilo Canguçu <daniloccangucu@gmail.com>
2024-03-18 16:13:18 +0100

    server connecting to mongodb

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,8 @@ import "dotenv/config";
 
 import carsRouter from "./routers/carsRouter";
 import authRouter from "./routers/authRouter";
+import usersRouter from "./routers/usersRouter";
 
-const PORT = 8080;
 const baseUrl = "/api/v1";
 
 const app = express();
@@ -12,9 +12,6 @@ app.use(express.json());
 
 app.use(`${baseUrl}/cars`, carsRouter);
 app.use(`${baseUrl}/auth`, authRouter);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.use(`${baseUrl}/users`, usersRouter);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,16 @@
-// connect database
-// password: ngocanhfs17
-// mongodb+srv://ngocanhhsgs:<password>@cluster0.rnslrmg.mongodb.net/
-
 import mongoose from "mongoose";
 import app from "./app";
 
-// get database url
 const mongodbUrl = process.env.MONGODB_URL as string;
 const port = process.env.PORT as string;
-// library: mongoose
 
 mongoose
   .connect(mongodbUrl, {
-    dbName: "sample_mflix",
+    dbName: "vintagecarsmarket",
   })
   .then(() => {
-    // logic
     app.listen(port, () => {
-      console.log("Database is connected");
+      console.log("Database vintagecarsmarket is connected");
       console.log(`Server is running on http://localhost:${port}`);
     });
   })
