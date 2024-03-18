3c3111667afbc553a8a81bba1f0d72a8867b5891
Danilo Canguçu <daniloccangucu@gmail.com>
2024-03-18 16:17:23 +0100

    cleaning old files

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config";
 
-import carsRouter from "./routers/carsRouter";
+// import carsRouter from "./routers/carsRouter";
 import authRouter from "./routers/authRouter";
 import usersRouter from "./routers/usersRouter";
 
@@ -10,7 +10,7 @@ const baseUrl = "/api/v1";
 const app = express();
 app.use(express.json());
 
-app.use(`${baseUrl}/cars`, carsRouter);
+// app.use(`${baseUrl}/cars`, carsRouter);
 app.use(`${baseUrl}/auth`, authRouter);
 app.use(`${baseUrl}/users`, usersRouter);
 
