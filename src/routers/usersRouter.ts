7d2be7c55ff99ee73a678672c0fa5871c12ba62b
Francis Eboyie <80025685+Eboyie@users.noreply.github.com>
2024-04-03 16:50:20 +0300

    added user operations

--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -1,27 +1,39 @@
-import express from "express";
-import ordersRouter from "./ordersRouter";
-import { hasPermission } from "../middlewares/hasPermission";
-import { isRequestedUser } from "../middlewares/isRequestedUser";
+import express from 'express';
+import ordersRouter from './ordersRouter';
+import { hasPermission } from '../middlewares/hasPermission';
+import { isRequestedUser } from '../middlewares/isRequestedUser';
 import {
   banUserHandler,
   changePasswordHandler,
   recoverPasswordHandler,
-} from "../controllers/usersController";
-import { isAdmin } from "../middlewares/isAdmin";
+  getAllUsers,
+  deleteUser,
+  getUserByIdHandler,
+  updateUser,
+} from '../controllers/usersController';
+import { isAdmin } from '../middlewares/isAdmin';
 
 const usersRouter = express.Router();
 
-usersRouter.use("/:userId/orderlists", hasPermission, ordersRouter);
+usersRouter.use('/:userId/orderlists', hasPermission, ordersRouter);
 usersRouter.get(
-  "/:userId/recover-password",
+  '/:userId/recover-password',
   isRequestedUser,
   recoverPasswordHandler
 );
 usersRouter.post(
-  "/:userId/change-password",
+  '/:userId/change-password',
   isRequestedUser,
   changePasswordHandler
 );
-usersRouter.patch("/:userId/ban", isAdmin, banUserHandler);
+usersRouter.patch('/:userId/ban', isAdmin, banUserHandler);
+//
+usersRouter.get('/', getAllUsers);
+
+usersRouter.delete('/:userId', hasPermission, deleteUser);
+
+usersRouter.get('/:userId', hasPermission, getUserByIdHandler);
+
+usersRouter.put('/:userId', hasPermission, updateUser);
 
 export default usersRouter;
