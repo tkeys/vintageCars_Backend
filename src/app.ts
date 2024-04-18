601faa5066d46256c73144d3f559871039b026db
Theokeys <theolarsenn@gmail.com>
2024-04-18 10:21:47 +0300

    databes updated and cors dependency installed

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import "dotenv/config";
 import carsRouter from "./routers/carsRouter";
 import authRouter from "./routers/authRouter";
 import usersRouter from "./routers/usersRouter";
+import cors from "cors";
 
 const baseUrl = "/api/v1";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(`${baseUrl}/cars`, carsRouter);
