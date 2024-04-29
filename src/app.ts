7ac06e58f31462b083f01bcf026940c58b4955c0
Theokeys <theolarsenn@gmail.com>
2024-04-29 09:27:01 +0300

    Vercel prep done

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import carsRouter from "./routers/carsRouter";
 import authRouter from "./routers/authRouter";
 import usersRouter from "./routers/usersRouter";
-import orderRouter from "./routers/orderRouter";
+//import orderRouter from "./routers/orderRouter";
 import cors from "cors";
 
 const baseUrl = "/api/v1";
@@ -16,6 +16,6 @@ app.use(express.json());
 app.use(`${baseUrl}/cars`, carsRouter);
 app.use(`${baseUrl}/auth`, authRouter);
 app.use(`${baseUrl}/users`, usersRouter);
-app.use(`${baseUrl}/orders`, orderRouter);
+//app.use(`${baseUrl}/orders`, orderRouter);
 
 export default app;
