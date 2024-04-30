65f0ee99a0a676c3ac2341646fc56aa5dcc16b31
Theokeys <theolarsenn@gmail.com>
2024-04-30 14:15:13 +0300

    cors updaed

--- src/app.ts
+++ src/app.ts
@@ -11,37 +11,11 @@ const baseUrl = "/api/v1";
 
 const app = express();
 
-const allowedOrigins = [
-  /* "http://localhost:3000", */
-  "https://fs17-e-commerce-project.vercel.app/",
-];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS policy violation"));
-      }
-    },
-    /* origin: [
-      "http://localhost:3000",
-      "https://fs17-e-commerce-project.vercel.app/api/v1",
-    ], */
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Origin",
-      "X-Requested-With",
-      "Accept",
-      "x-client-key",
-      "x-client-token",
-      "x-client-secret",
-      "Authorization",
-    ],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: "https://fs17-e-commerce-project.vercel.app",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(`${baseUrl}/cars`, carsRouter);
