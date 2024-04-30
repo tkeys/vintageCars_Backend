fe42b6fcdf9ffc19e8e8c3940a2d7222525a654b
Theokeys <theolarsenn@gmail.com>
2024-04-30 13:00:25 +0300

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,24 @@ import cors from "cors";
 const baseUrl = "/api/v1";
 
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://fs17-e-commerce-project.vercel.app/api/v1",
+];
 app.use(
   cors({
-    origin: [
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS policy violation"));
+      }
+    },
+    /* origin: [
       "http://localhost:3000",
       "https://fs17-e-commerce-project.vercel.app/api/v1",
-    ],
+    ], */
     credentials: true,
   })
 );
